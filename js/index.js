389c57b67def020458b47760863bc0fbf830e143
Sergio Franco <checho1536@gmail.com>
2022-04-18 22:55:48 -0500

    Sitio terminado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 document.querySelector('.btn-menu').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+function limpiarTabla() {
+    const tbody = document.querySelector('#body-result');
+    const trs = tbody.childNodes;
+    trs.forEach((result, index) => {
+        const tr = document.querySelector('#row-result');
+        tbody.removeChild(tr);
+    });
+    
+}
